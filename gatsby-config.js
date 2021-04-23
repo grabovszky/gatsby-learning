10bc5381d4e2a401d52ed22128102bd2a300d4d0
grabovszky <gaborkristof@protonmail.com>
2021-04-23 08:31:13 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+  siteMetadata: {
+    title: "Web Warrior",
+    description: "web dev portfolio",
+    copyright: "This website is copyright 2021 Web Warrior",
+    contact: "info@webwarrior.com",
+  },
+}
---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Excepteur Lorem ex sit dolore pariatur est tempor dolore veniam sunt
+          ipsum.
+        </p>
+        <p>
+          Excepteur Lorem ex sit dolore pariatur est tempor dolore veniam sunt
+          ipsum.
+        </p>
+        <p>
+          Excepteur Lorem ex sit dolore pariatur est tempor dolore veniam sunt
+          ipsum.
+        </p>
+      </div>
+    </Layout>
+  )
+}
